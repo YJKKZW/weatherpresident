ff4947d1738aa5166f9d7f7d5562420f8a8b8978
Yoji Kakizawa <kakizawayoji@yahoo.co.jp>
2017-11-24 15:53:10 +0900

    20171124

--- main.py
+++ main.py
@@ -2,29 +2,48 @@
 import urllib2, sys
 import json
 import webapp2
+import os
 
-class HelloWebapp2(webapp2.RequestHandler):
+import jinja2
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, html, values={}):
+        template = JINJA_ENVIRONMENT.get_template(html)
+        self.response.write(template.render(values))
+
+class MainHandler(BaseHandler):
     def get(self):
         resp = urllib2.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010').read()
         resp = json.loads(resp)
 
-        html  = u''
-        html += resp['forecasts'][0]['date']
-        html += u'の東京の天気は<span style="color:blue">'
-        html += resp['forecasts'][0]['telop']
-        html += u'</span>です'
-        self.response.write(html)
+        template = JINJA_ENVIRONMENT.get_template("index.html")
+        template_vars = {
+        "weather":resp['forecasts'][0]['telop'],
+        "date":resp['forecasts'][0]['date'],
+        }
+        self.response.write(template.render(template_vars))
 
-class HelloWebapp2Hoge(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello, Glossom Get')
-    def post(self):
-        self.response.write('Hello, Glossom Post')
+#        sitename  = u''
+#        sitename += resp['forecasts'][0]['date']
+#        html += u'の東京の天気は<span style="color:blue">'
+#        html += resp['forecasts'][0]['telop']
+#        html += u'</span>です'
+#        self.response.write(html)
 
-class Weather(webapp2.RequestHandler):
-    def get(self):
-        citycode = '130010' #tokyo
-        resp = urllib2.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%citycode).read()
+#class HelloWebapp2Hoge(webapp2.RequestHandler):
+#    def get(self):
+#        self.response.write('Hello, Glossom Get')
+#    def post(self):
+#        self.response.write('Hello, Glossom Post')
+
+#class Weather(webapp2.RequestHandler):
+#    def get(self):
+#        citycode = '130010' #tokyo
+#        resp = urllib2.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%citycode).read()
 
         ## JSON自体を表示
         #self.response.headers['Content-Type'] = 'application/json'
@@ -32,24 +51,23 @@ class Weather(webapp2.RequestHandler):
 
         # 文字列で表示
         # 読み込んだJSONデータをディクショナリ型に変換
-        self.response.headers['Content-Type'] = 'text/plain'
-        resp = json.loads(resp)
-        output = u"citycodeは%sです\n" % citycode
-        output += "**************************\n"
-        output += resp['title']
-        output += "\n**************************\n"
-        output += resp['description']['text']
-        output += "\n"
+#        self.response.headers['Content-Type'] = 'text/plain'
+#        resp = json.loads(resp)
+#        output = u"citycodeは%sです\n" % citycode
+#        output += "**************************\n"
+#        output += resp['title']
+#        output += resp['description']['text']
+#        output += "\n"
 
-        for forecast in resp['forecasts']:
-            output += "**************************\n"
-            output += forecast['dateLabel'] + '(' + forecast['date'] + ')' + "\n"
-            output += forecast['telop'] + "\n"
-        output += "**************************\n"
-        self.response.out.write(output)
+#        for forecast in resp['forecasts']:
+#            output += "**************************\n"
+#            output += forecast['dateLabel'] + '(' + forecast['date'] + ')' + "\n"
+#            output += forecast['telop'] + "\n"
+#        output += "**************************\n"
+#        self.response.out.write(output)
 
 app = webapp2.WSGIApplication([
-    ('/', HelloWebapp2),
-    ('/hoge', HelloWebapp2Hoge),
-    ('/weather', Weather),
+    ('/', MainHandler),
+#    ('/hoge', HelloWebapp2Hoge),
+#    ('/weather', Weather),
 ], debug=True)
